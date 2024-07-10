fa7be9dd11c5ea5df06716ff18d09d7151902e01
Jundi Mubarok <57140394+jundi14@users.noreply.github.com>
2024-07-10 10:47:05 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,8 +31,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={'Belanja Akhir Tahun'}
+        subtitle={'Belanja Akhir Tahun Lebih Hemat!'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
